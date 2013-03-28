2e1f0f3fa760d6111bcea3834353dd9f52bd7d63
Olli Rissanen <olli.rissanen@helsinki.fi>
2013-03-28 10:18:39 +0200

    Added comments for folder exclusion and logging

--- pull.sh
+++ pull.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
-#Iterates your workspace folders and calls git pull on each
+#Iterates your workspace folders and calls git pull on each git project
 
 #Replace with your workspace path
 WORKSPACE="/c/workspace/workspace_git/"
 
 cd $WORKSPACE
+echo -e "Pulling for $WORKSPACE"
 
-echo -e "\nPulling down latest for $WORKSPACE\n"
-
-for d in $(find . -maxdepth 1 -type d); do
-		#Add excludes for folders here, e.g.
-		#if [[ "$d" == *foldername* ]]; ..
-		#to either skip or visit certain folders
-        echo -e "$d"
-        cd $d
-        git remote -v
-        git pull
-        cd $WORKSPACE
+for dir in $(find . -maxdepth 1 -type d); do
+	#Add excludes for folders here, e.g.
+	#if [[ "$d" == *foldername* ]]; ..
+	#to either skip or visit certain folders	
+	cd $dir
+	if [ -d .git ]; then
+		echo $dir | cut -d'/' -f 2
+		git pull		
+		#To print origin uncomment the following line
+		#git remote -v
+		#To exclude up-to-date messages uncomment the following lines
+		#R=$(git pull 2>&1)
+		#if [ "$R" != "Already up-to-date." ]; then
+			#echo $R
+		#fi
+	fi
+	cd $WORKSPACE
 done
 
 echo -e "\nFinished.\n"
